66d1dd690b645b5e5ceb2bfc054b08277551c724
Roman Diakonov <vegappunk@gmail.com>
2025-02-12 08:56:32 +0300

    chore: Delete localization files

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_anime_app/core/app/app_router.dart';
 import 'package:flutter_anime_app/core/app/app_theme.dart';
-import 'package:flutter_anime_app/generated/l10n.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 final globalKey = GlobalKey();
 
@@ -13,15 +11,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
     return MaterialApp.router(
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const <Locale>[
-        Locale.fromSubtags(languageCode: 'ru'),
-      ],
       key: globalKey,
       title: 'FlutterAnimeApp',
       theme: appTheme,
