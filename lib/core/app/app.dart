e7edc619075305923637c384ad69e14fc5613619
Roman Diakonov <vegappunk@gmail.com>
2025-02-13 22:39:53 +0300

    chore: Add MultiBlocProvider

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_anime_app/core/app/app_router.dart';
 import 'package:flutter_anime_app/core/app/app_theme.dart';
+import 'package:flutter_anime_app/core/di/injectable.dart';
+import 'package:flutter_anime_app/presentation/cubits/anime_categories_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,11 +11,16 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
-    return MaterialApp.router(
-      title: 'FlutterAnimeApp',
-      theme: appTheme,
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter.config(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AnimeCategoriesCubit>(create: (context) => getIt()),
+      ],
+      child: MaterialApp.router(
+        title: 'FlutterAnimeApp',
+        theme: appTheme,
+        debugShowCheckedModeBanner: false,
+        routerConfig: appRouter.config(),
+      ),
     );
   }
 }
