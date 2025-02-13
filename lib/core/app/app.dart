8877e7cef4ae7a3dfaa94ad772ca34ec253d96b0
Roman Diakonov <vegappunk@gmail.com>
2025-02-13 21:54:37 +0300

    fix: UI

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_anime_app/core/app/app_router.dart';
 import 'package:flutter_anime_app/core/app/app_theme.dart';
 
-final globalKey = GlobalKey();
-
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -11,7 +9,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
     return MaterialApp.router(
-      key: globalKey,
       title: 'FlutterAnimeApp',
       theme: appTheme,
       debugShowCheckedModeBanner: false,
