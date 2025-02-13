331dac82d6194740cce768ce57309e51670f349f
Roman Diakonov <vegappunk@gmail.com>
2025-02-14 02:37:21 +0300

    feat: Add font Comfortaa

--- lib/core/app/app_theme.dart
+++ lib/core/app/app_theme.dart
@@ -1,31 +1,15 @@
 import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
+  fontFamily: 'Comfortaa',
   primaryColor: Colors.blueAccent,
-  // scaffoldBackgroundColor: Color(0xFFecf0f1),
+  canvasColor: Colors.white,
   scaffoldBackgroundColor: Colors.white,
-  appBarTheme: AppBarTheme(
-    iconTheme: IconThemeData(
-      color: Colors.white,
-    ),
-    titleTextStyle: TextStyle(color: Colors.white),
-  ),
   textTheme: TextTheme(
     headlineLarge: TextStyle(
         fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
     headlineMedium: TextStyle(
         fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold,),
     headlineSmall: TextStyle(fontSize: 14, color: Colors.grey),
-  ),
-  cardTheme: CardTheme(
-    color: Colors.white,
-    shadowColor: Colors.black,
-    elevation: 6,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(16),
-    ),
-  ),
-  buttonTheme: ButtonThemeData(
-    textTheme: ButtonTextTheme.primary,
   ),
 );
