47d57747cb240c840631319161eb10cafd89f0ea
Roman Diakonov <vegappunk@gmail.com>
2025-03-09 23:04:20 +0300

    chore: added appmetrica and env

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Constants {
   static const String baseUrl = "https://api.rony.fun/";
   static const Duration previewAnimatedDuration = Duration(seconds: 2);
+
+  static String? appMetricaApiKey = dotenv.env['APP_METRICA_KEY'] ?? "";
 }
